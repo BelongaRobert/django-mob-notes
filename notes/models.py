b08238f69aa2c1d199fefae4339522d2c322b567
shelby_soutter <ssa.soutter@gmail.com>
2020-07-06 10:04:30 -0400

    initial class setup

---
+++ notes/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from users.models import User
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+
+
+
+
+class Note(models.Model):
+    text = models.TextField()
+    title = models.CharField(max_length=255)
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_edited = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+    catgories = models.ManyToManyField(Category, on_delete=models.DO_NOTHING, related_name='notes')
+
+
