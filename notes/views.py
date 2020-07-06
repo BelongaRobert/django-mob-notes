a8cf07f69925c5f1e49d31db40926abc92ea199b
Blake <blakeevans336@gmail.com>
2020-07-06 10:59:48 -0400

    add vieww

--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Note, Category
 from .forms import NoteForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def list_notes(request):
-    pass
+    user = request.user
+    notes = user.notes.all()
+    return render(request,  'notes/list_notes.html', {'notes': notes})
+
+
+
+    
