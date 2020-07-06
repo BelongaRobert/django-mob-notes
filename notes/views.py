d640bc3de101343c992c270f4218a4d659fe0a0a
Rh017548 <russellhill752@msn.com>
2020-07-06 10:18:06 -0400

    added urls

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Note, Category
+from .forms import NoteForm
+
+
 
 # Create your views here.
