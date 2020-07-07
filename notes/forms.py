992e4e372b6ab550a5a6bf27250c7934b7fbc31c
Amy Gori <amydgori@gmail.com>
2020-07-07 18:05:22 -0400

    Improve adding classes to form widgets and other styling

--- notes/forms.py
+++ notes/forms.py
@@ -2,6 +2,12 @@ from django.forms import ModelForm, Textarea
 from .models import Note, Category
 
 class NoteForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(ModelForm, self).__init__(*args, **kwargs)
+        self.fields['text'].widget.attrs['class'] = 'db border-box hover-black w-100 measure ba b--black-20 pa2 br2 mv2'
+        self.fields['title'].widget.attrs['class'] = 'input-reset ba b--black-20 pa2 mb2 db w-100'
+        self.fields['categories'].widget.attrs['class'] = 'input-reset w-100 mv2 b--black-20'
     class Meta:
         model = Note
         fields = [
@@ -10,9 +16,7 @@ class NoteForm(ModelForm):
             'categories',
         ]
 
-        widgets = {
-            'text': Textarea(attrs={'class': 'note-text'}),
-        }
+
 
 
 
